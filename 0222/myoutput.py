32b740580ee6d12d1e1be622c8109ce03949fc1d
g2ro <115413938+g2ro@users.noreply.github.com>
2024-04-29 20:52:08 +0900

    0222_DGB
    0222대구은행

---
+++ 0222/myoutput.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def output_student(student):
+    print('이름 = %s, 나이 = %s입니다.'%(student['name'],student['age']))
+
---
+++ 0222/util.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#util.ipynb
+INCH = 2.54 #파이썬에는 상수라는 개념이 존재x
+
+def calcSum():
+    sum = 0
+    for num in range(1, 101):
+        sum += num
+    return sum
+
